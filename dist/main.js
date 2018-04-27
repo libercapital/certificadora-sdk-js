ce8163cd5cf0776c0cf7b1746ab13c13890475db
senechal <andre.senechal@hotmail.com>
2018-04-27 10:19:16 -0300

    feat(Dist): Add compiled distribution
    Add commitizen for more organized commits;
    Add compiled distribution of the module.

---
+++ dist/main.js
@@ -0,0 +1,147 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _utils = require('./utils.js');
+
+var _utils2 = babelHelpers.interopRequireDefault(_utils);
+
+var _support = require('./support.js');
+
+var _support2 = babelHelpers.interopRequireDefault(_support);
+
+/*
+ * http://jscc.info/
+ * Chrome: 33-57
+ * Edge: 12-14
+ * Firefox 29-52
+ * IE: n/a
+ * iOS Safari: 8-10
+ * Opera: 20-43
+ * Safari: 7.1-
+ */
+// import Promise from '../node_modules/promise/polyfill.js';
+var SDK = function (Utils, Support) {
+  'use strict';
+
+  var _ = {
+    STATES: {
+      IDLE: -1,
+      READY: 0,
+      EXTENSION_MISSING: 1,
+      EXTENSION_MAJOR: 2,
+      EXTENSION_MINOR: 3,
+      NATIVE_MISSING: 4,
+      NATIVE_MAJOR: 5,
+      NATIVE_MINOR: 6,
+      NATIVE_ERROR: 7
+    },
+    Support: Support,
+    Utils: Utils
+  },
+      status = _.STATES.IDLE,
+      hasExtension = false,
+      hasNative = false,
+      isExtended = false;
+
+  _.status = function () {
+    return status;
+  };
+
+  _.hasExtension = function () {
+    return hasExtension;
+  };
+
+  _.hasNative = function () {
+    return hasNative;
+  };
+
+  _.extensionCheck = function () {
+    return new Promise(function (resolve, reject) {
+      // check if extension's library is registered on global scope
+      if (typeof window.AssinaMeExtension === 'undefined') {
+        hasExtension = false;
+        status = _.STATES.EXTENSION_MISSING;
+        reject(new Error('Extension is missing'));
+
+        return;
+      }
+
+      hasExtension = true;
+
+      // check if library's major version is at least 0
+      if (window.AssinaMeExtension.version.MAJOR < 0) {
+        status = _.STATES.EXTENSION_MAJOR;
+        reject(new Error('Extension major version is not supported'));
+
+        return;
+      }
+
+      // check if library's minor version is at least 1
+      if (window.AssinaMeExtension.version.MINOR < 1) {
+        status = _.STATES.EXTENSION_MINOR;
+        reject(new Error('Extension minor version is not supported'));
+
+        return;
+      }
+
+      if (!isExtended) {
+        isExtended = true;
+
+        // add AssinaMeExtension to _ scope
+        _ = Object.assign(_, window.AssinaMeExtension);
+      }
+
+      resolve(_.version.MAJOR + '.' + _.version.MINOR);
+    });
+  };
+
+  _.nativeCheck = function (timeout) {
+    return new Promise(function (resolve, reject) {
+      if (typeof window.AssinaMeExtension === 'undefined') {
+        hasExtension = false;
+        status = _.STATES.EXTENSION_MISSING;
+        reject(new Error('Extension is missing'));
+
+        return;
+      }
+
+      // check if native app is installed and version requirements
+      window.AssinaMeExtension.nativeVersion(timeout).then(function (response) {
+        hasNative = true;
+        // check if native's major version is at least 0
+        if (response.MAJOR < 0) {
+          status = _.STATES.NATIVE_MAJOR;
+          reject(new Error('Native App major version is not supported'));
+
+          return;
+        }
+
+        // check if native's minor version is at least 1
+        if (response.MINOR < 1) {
+          status = _.STATES.NATIVE_MINOR;
+          reject(new Error('Native App minor version is not supported'));
+
+          return;
+        }
+
+        status = _.STATES.READY;
+        resolve(response.MAJOR + '.' + response.MINOR);
+      }, function () {
+        hasNative = false;
+        status = _.STATES.NATIVE_MISSING;
+        reject(new Error('Native App is missing'));
+      });
+    });
+  };
+
+  _.systemCheck = function (timeout) {
+    return Promise.all([_.extensionCheck(), _.nativeCheck(timeout)]);
+  };
+
+  return _;
+}(_utils2.default, new _support2.default(_utils2.default.currentBrowser(), _utils2.default.currentOS()));
+
+exports.default = SDK;
---
+++ dist/utils.js
@@ -0,0 +1,148 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _bowser = require('bowser');
+
+var _bowser2 = babelHelpers.interopRequireDefault(_bowser);
+
+var Utils = function (bowser) {
+  'use strict';
+
+  var _ = {};
+
+  _.currentPlatform = function () {
+    if (bowser.mobile) {
+      return 'mobile';
+    }
+
+    if (bowser.tablet) {
+      return 'tablet';
+    }
+
+    return 'computer';
+  };
+
+  _.platformDisplayName = function (platformName) {
+    platformName = platformName || _.currentPlatform();
+
+    switch (platformName) {
+      case 'mobile':
+        return 'Mobile';
+      case 'tablet':
+        return 'Tablet';
+      case 'computer':
+        return 'Computer';
+      default:
+        return 'Unknown';
+    }
+  };
+
+  _.currentOS = function () {
+    if (bowser.mac) {
+      return 'mac';
+    }
+
+    if (bowser.windows) {
+      return 'windows';
+    }
+
+    if (bowser.linux) {
+      return 'linux';
+    }
+
+    return 'unknown';
+  };
+
+  _.osDisplayName = function (OSName) {
+    OSName = OSName || _.currentOS();
+
+    switch (OSName) {
+      case 'windows':
+        return 'Microsoft Windows';
+      case 'mac':
+        return 'macOS';
+      case 'linux':
+        return 'Linux';
+      default:
+        return 'Unknown';
+    }
+  };
+
+  _.currentBrowser = function () {
+    if (bowser.chrome) {
+      return 'chrome';
+    }
+
+    if (bowser.firefox) {
+      return 'firefox';
+    }
+
+    if (bowser.msedge) {
+      return 'edge';
+    }
+
+    return 'unknown';
+  };
+
+  _.browserDisplayName = function (browserName) {
+    browserName = browserName || _.currentBrowser();
+
+    switch (browserName) {
+      case 'chrome':
+        return 'Google Chrome';
+      case 'firefox':
+        return 'Mozilla Firefox';
+      default:
+        return 'Unknown';
+    }
+  };
+
+  _.extensionPage = function (browserName) {
+    browserName = browserName || _.currentBrowser();
+
+    switch (browserName) {
+      case 'chrome':
+        return 'chrome://extensions';
+      case 'firefox':
+        return 'about:addons';
+      default:
+        return '#';
+    }
+  };
+
+  _.extensionLink = function (browserName) {
+    browserName = browserName || _.currentBrowser();
+
+    switch (browserName) {
+      case 'chrome':
+        return 'https://chrome.google.com/webstore/detail/liber-certificadora/jmafcfholebmbmaclbmdfidkgikmjklo';
+      case 'firefox':
+        return 'https://addons.mozilla.org/en-US/firefox/addon/assina-me/';
+      default:
+        return '#';
+    }
+  };
+
+  _.nativeLink = function (OSName) {
+    OSName = OSName || _.currentOS();
+
+    switch (OSName) {
+      case 'windows':
+        return 'https://s3.amazonaws.com/liber-certificadora-files/assina-me.exe';
+      case 'mac':
+        return 'https://s3.amazonaws.com/liber-certificadora-files/assina-me.pkg';
+      default:
+        return '#';
+    }
+  };
+
+  _.supportLink = function () {
+    return 'https://certificadora.libercapital.com.br/suporte';
+  };
+
+  return _;
+}(_bowser2.default); // import * as bowserjs from '../node_modules/bowser/bowser.js';
+exports.default = Utils;
